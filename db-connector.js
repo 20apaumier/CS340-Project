ee54519fc0e315a23ffe2b509e1392cc9a0ea71e
20apaumier <39337835+20apaumier@users.noreply.github.com>
2023-08-02 21:21:41 -0700

    Initial Commit

---
+++ db-connector.js
@@ -0,0 +1,18 @@
+// import env for encrypytion
+require('dotenv').config();
+
+// Get an instance of mysql we can use in the app
+var mysql = require('mysql')
+
+
+// Create a 'connection pool' using the provided credentials
+var pool = mysql.createPool({
+    connectionLimit : 10,
+    host            : 'classmysql.engr.oregonstate.edu',
+    user            : process.env.USER,
+    password        : process.env.DB_PASSWORD,
+    database        : process.env.DATABASE
+})
+
+// Export it for use in our application
+module.exports.pool = pool;
